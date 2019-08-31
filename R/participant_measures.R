6f18a8495373d6f9c61c374689758172ac8e57f2
Jascha Grübel <jgruebel@student.ethz.ch>
2019-08-31 18:03:41 +0200

    Fix JRD measures

--- R/participant_measures.R
+++ R/participant_measures.R
@@ -30,13 +30,12 @@ compute_jrd_3d_error <- function(jrd_data, destinations){
   y <- destinations[jrd_data$Position,2:4]-destinations[jrd_data$Goal,2:4]
 
   dt2 <- cbind(x,y)
-  rownames(dt2)<-jrd_data$description
 
-  jrd_data$correct_angles <- apply(dt2,1,function(a){angle_2d(a[c(1,3)],a[c(4,6)])})
+  jrd_data$correct_angles <- apply(dt2,1,function(a){angle_2d(a[c(4,6)],a[c(1,3)])})
 
-  jrd_data$value <- abs(with(jrd_data, ifelse(value > 180,value-360,value)))
+  jrd_data$error <- abs(with(jrd_data, ifelse(value-correct_angles > 180,value-360-correct_angles,value-correct_angles)))
 
-  return(with(jrd_data,value))
+  return(with(jrd_data,error))
 }
 
 #' Compute JRD 2D Error
@@ -72,9 +71,8 @@ compute_jrd_2d_error <- function(jrd_data, destinations){
   y <- destinations[jrd_data$Position,2:4]-destinations[jrd_data$Goal,2:4]
 
   dt2 <- cbind(x,y)
-  rownames(dt2)<-jrd2_data$description
 
-  jrd_data$correct_angles <- apply(dt2,1,function(a){angle_2d(a[c(1,3)],a[c(4,6)])})
+  jrd_data$correct_angles <- apply(dt2,1,function(a){angle_2d(a[c(4,6)],a[c(1,3)])})
   jrd_data$error <- with(jrd_data,ifelse(value==1,correct_angles-45,
                                            ifelse(value==2,correct_angles-90,
                                                   ifelse(value==3,correct_angles-135,
@@ -84,9 +82,9 @@ compute_jrd_2d_error <- function(jrd_data, destinations){
                                                                               ifelse(value==4  & correct_angles <0,correct_angles+180,correct_angles-180))))))))
 
   jrd_data$error2 <- with(jrd_data,ifelse(error > -22.5 & error < 22.6,0,
-                                            ifelse(error > -67.5 & error < 67.5,ifelse(error < 0,-1,1),
-                                                   ifelse(error > -112.5 & error < 112.5,ifelse(error < 0,-2,2),
-                                                          ifelse(error > -147.5 & error < -147.5,3,4)))))
+                                            ifelse(error > -67.5 & error < 67.5,ifelse(error > 0,-1,1),
+                                                   ifelse(error > -112.5 & error < 112.5,ifelse(error > 0,-2,2),
+                                                          ifelse(error > -147.5 & error < -147.5,ifelse(error > 0,-3,3),4)))))
 
   return(with(jrd_data,error2))
 }
